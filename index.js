1ec5f3bc5db93832431ee326a69991daa92a8148
Gleiton Brito Bernardes <154294770+Gleitonbb@users.noreply.github.com>
2024-03-26 22:54:59 -0300

    em construção

---
+++ index.js
@@ -0,0 +1,8 @@
+const caixaEscolhido = document.querySelector("#caixaEscolhido")
+const caixaMae = document.querySelector("#caixamae")
+const Flamengo = document.querySelector("#fla")
+const Palmeiras = document.querySelector("#pal")
+const flaPal = [...Flamengo, Palmeiras]
+flaPal.map((upu, pos)=>{
+    console.log(upu)
+})
